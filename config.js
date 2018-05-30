fbfb15fb339417d191aa427423d3c0227194a661
吴礼杰 <wu453973171@163.com>
2018-05-30 15:22:57 +0800

    优化:更改mysql 地址
    Signed-off-by: 吴礼杰 <wu453973171@163.com>

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var configs     = exports;
 
 configs.mysql = {
-    MYSQL_HOST  :     '10.23.102.238',
+    MYSQL_HOST  :     '10.23.60.2',
     MYSQL_PORT  :     3306,
     MYSQL_USER  :     'root',
     MYSQL_PSWD  :     'Wulijie123',
